42fe394cdc38fab09b1168fd8a7875c676ea3163
Derpmaster45 <31549635+Derpmaster45@users.noreply.github.com>
2022-04-15 17:56:38 -0500

    added to game.cpp and function file
    Main gameplay loop is complete but need make changes to number limits

--- Guess_the_number/game.cpp
+++ Guess_the_number/game.cpp
@@ -4,5 +4,17 @@
 
 int main ()
 {
+    int difficulty=1;
+    int maxDifficulty=10;
+
+    // run the game until difficulty equals max difficulty
+    while(difficulty<=maxDifficulty)
+    {
+        PlayGame(difficulty);
+        difficulty++;
+        std::cin.ignore();
+        std::cin.clear();
+        std::cout.clear();
+    }
     return 0;
 }
